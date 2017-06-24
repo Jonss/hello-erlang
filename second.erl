808473165036c30f6c8aeaf626860efa2c4a2ce9
Jonss <joaosantana.ti@gmail.com>
2017-06-24 15:22:22 -0300

    Updating second module to reuse hypotenuse function

--- second.erl
+++ second.erl
@@ -1,6 +1,6 @@
 -module(second).
 
--export([hypotenuse/2, right_triangle_area/2, right_triangle_perimeter/3]).
+-export([hypotenuse/2, right_triangle_area/2, right_triangle_perimeter/2]).
 
 hypotenuse(A,B) -> 
 		math:sqrt(first:square(A) + first:square(B)).
@@ -8,5 +8,5 @@ hypotenuse(A,B) ->
 right_triangle_area(A,B) ->
 	(1/2) * (A * B).
 
-right_triangle_perimeter(A,B,C) ->
-	A + B + C.
+right_triangle_perimeter(A,B) ->
+	A + B + hypotenuse(A, B).
